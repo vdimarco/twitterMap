f8090845bd337bb8cb5d681ce98e3d6c444bdf86
Vaughn DiMarco <vaughn.dimarco@gmail.com>
2013-11-07 21:38:53 -0500

    fixed OAuth issues

--- twitterMap.R
+++ twitterMap.R
@@ -2,24 +2,22 @@
 # Source the function
 source("http://biostat.jhsph.edu/~jleek/code/twitterMap.R")
 
-reqURL <- "http://api.twitter.com/oauth/request_token"
-accessURL <- "http://api.twitter.com/oauth/access_token"
-authURL <- "http://api.twitter.com/oauth/authorize"
 ck <- 'cmxCCQCSzjz9RQ8UOkv7g'
 cs <- 'ptvTcfzGBupFeGbgceK4UAmUoesbiV5dEvmZ94dOPVU'
+getTwitterOAuth(ck, cs)
 
-options(RCurlOptions = list(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl")))
-download.file(url="http://curl.haxx.se/ca/cacert.pem", destfile="cacert.pem")
+# reqURL <- "http://api.twitter.com/oauth/request_token"
+# accessURL <- "http://api.twitter.com/oauth/access_token"
+# authURL <- "http://api.twitter.com/oauth/authorize"
+# options(RCurlOptions = list(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl")))
+# download.file(url="http://curl.haxx.se/ca/cacert.pem", destfile="cacert.pem")
+# 
+# twitCred <- OAuthFactory$new(consumerKey=ck, consumerSecret=cs, 
+#                              requestURL=reqURL, accessURL=accessURL, authURL=authURL)
+# 
+# twitCred$handshake(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))
+# registerTwitterOAuth(twitCred)
 
-twitCred <- OAuthFactory$new(consumerKey=ck,
-                             consumerSecret=cs,
-                             requestURL=reqURL,
-                             accessURL=accessURL,
-                             authURL=authURL)
-twitCred$handshake(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))
-registerTwitterOAuth(twitCred)
-
-# getTwitterOAuth(ck, cs)
 
 # Make your twittermap
 twitterMap("vdimarco")
@@ -31,7 +29,7 @@ data(world.cities)
 grep("Montreal",world.cities[,1])
 
 # Then make the map using that big city
-twitterMap("simplystats",userLocation="Baltimore")
+twitterMap("vdimarco",userLocation="Montreal")
 
 #If you want both your followers and people you follow in a plot you can do:
-twitterMap("simplystats",plotType="both")
+twitterMap("vdimarco",userLocation="Montreal",plotType="both")
